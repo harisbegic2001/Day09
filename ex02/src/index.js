2531c3b8029f34ce1dbe71af065c0d33646bede6
Haris Begić <harisbegic01@hotmail.com>
2021-03-16 09:37:07 +0100

    2nd task Day09

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+//Only change code below this line
+function myFunction(myParameter) {
+  if (myParameter) {
+    return "The parameter is true!";
+  } else {
+    return "The parameter is false!";
+  }
+}
+console.log(myFunction(false));
+console.log(myFunction(true));
+
+//Only change code above this line
+module.exports = myFunction;
