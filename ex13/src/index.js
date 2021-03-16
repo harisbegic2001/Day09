52bc606e9e87e30792cf160a1f1d604c77c5f44c
Haris Begić <harisbegic01@hotmail.com>
2021-03-16 12:50:13 +0100

    13th task Day09

---
+++ ex13/src/index.js
@@ -0,0 +1,14 @@
+//Only change code below this line
+function inverseWhile(){
+    var fiveNumbers = 5;
+    while(fiveNumbers > 0){
+        console.log(fiveNumbers);
+        fiveNumbers--;
+    }
+    return fiveNumbers;
+}
+
+
+//Only change code below this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
